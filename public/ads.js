b44171cf055b2917fe6cf1b34fed5dccbcbacc64
root <root@vmi263582.contaboserver.net>
2020-05-05 18:27:58 +0200

    .

---
+++ public/ads.js
@@ -0,0 +1,7 @@
+var a = document.createElement('div');
+a.innerHTML = '&nbsp;';
+a.id= 'ELZSYfwlTxzK';
+a.className = 'adsbox pub_300x250 pub_300x250m pub_728x90 text-ad textAd text_ad adglare ad-server';
+a.style = 'width: 1px !important; height: 1px !important; position: absolute !important; left: -10000px !important; top: -1000px !important;';
+document.body.appendChild(a);
+window.adBlockerDetectorLoaded=true;
